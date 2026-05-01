7ba280d867d7f69411d9124436a3224abbca5646
Ellkagaaa <enzo.morel1@yahoo.fr>
2026-05-01 11:21:42 +0000

    premier pas

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # Ici les imports 
+import pandas as pd
 
 def main():
     # C'est ici que tu dois remplir ton code
     print("Main program")
+    data = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+    
+    survived = data[data["Survived"] == 1]
+    non_survivant = data[data["Survived"] == 0]
+    print("\nSurvived")
+    print("\nnon_survivant")
+
+    data_sorted = data.sort_values(by="Name")
+    print(data_sorted["Name"])
+
 
 # On pourra voir ça ensemble mais ne t'occupe pas de ça
 if __name__ == "__main__":
