e6a9c9a03c717d44a00bcaac2ecbfb2995f5f354
Tom Poidevin <tom.poidevin@gmail.com>
2026-05-02 15:10:54 +0200

    Retour et examples de fonction

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 # Ici les imports 
 import pandas as pd
 
+def get_survived(data):
+    return data[data["Survived"] == 1]
+
+def get_dead_people(data):
+    return data[data["Survived"] == 0]
+
+def get_by_age_over(data, age):
+    return data[data["Age"] >= age]
+
+def get_peoples_name(data):
+    return data[data["Name"]]
+
+def get_people_by_genre(data, genre):
+    return data[data["Sex"] == genre]
+
+def print_new_line(str):
+    print("\n" + str)    
+
 def main():
     # C'est ici que tu dois remplir ton code
     print("Main program")
-    data = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
-    
-    survived = data[data["Survived"] == 1]
-    non_survivant = data[data["Survived"] == 0]
+    data = pd.read_csv("./titanic.csv")
+
+    survived = get_survived(data)
+    dead = get_dead_people(data)
 
     #Survivants
-    print("\nSurvived")
-    print(survived[["Name","Survived"]].head())
-    #Non-survivant
-    print("\nnon_survivant")
-    print(non_survivant[["Name", "Survived"]].head())    
-   
+    print_new_line("Survived")
+    # print(survived[["Name"]].head(12345).to_string(index=False, header=None))
+    print(survived[["Name"]].head().to_string(index=False, header=None))
+    # Non-survivant
+    print_new_line("Décédé")
+    print(dead[["Name", "Survived"]].head())
 
     
-
-
 # On pourra voir ça ensemble mais ne t'occupe pas de ça
 if __name__ == "__main__":
     main()
